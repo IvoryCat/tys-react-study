f3c83baf63ed8ded37a26847aed3d5d8aa5dcd9e
Kim Junsoo <blue3197@gmail.com>
2022-05-09 11:40:46 +0900

    react init

--- es6.js
+++ es6.js
@@ -1,1 +1,24 @@
-history;
+// var let const
+
+/* 
+1. 유효범위(scope)
+var : 함수단위 scope
+let, const : 블록단위
+
+2. 재할당 가능
+var, let : 가능
+const : 불가능
+*/
+
+for (var i = 0; i < 5; i++) {
+  console.log("안쪽" + i);
+}
+console.log("바깥쪽" + i);
+
+for (let j = 0; j < 5; j++) {
+  
+  console.log("안쪽" + j);
+  
+}
+console.log("바깥쪽" + j);
+
